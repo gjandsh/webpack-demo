eb34b1d7bcca03e8f846414e55eaac4511a804d2
gjandsh <gjandsh@126.com>
2017-07-09 16:09:39 +0800

    引入scss，引入图片、压缩:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import './index.scss';
+
+var $img1 = $('<img />');
+$img1[0].src=require("./1.jpg");
+$(document.body).append($img1);
+
 function component() {
 	  var $div = $('<div/>');
 
-	   $div.html(_.join(['Hello', 'World'], ' '));
+	   $div.html(_.join(['Hello', 'Summer'], ' '));
 	
      return $div;
      }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
   //入口
@@ -8,5 +10,39 @@ module.exports = {
     filename: 'bundle.js',	
   // __dirname指当前文件所在目录
     path: path.resolve(__dirname, 'dist')
-  }
+},
+  devServer:{
+	contentBase:path.resolve(__dirname,'dist'),
+	compress:true,
+	port:9000
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title:'webpack-demos',
+      fifename:'index.html'
+    }),
+    new OpenBrowserPlugin({
+      url:'http://localhost:9000'
+    })	  
+  ],
+  module:{
+    loaders: [
+      {
+	test:/\.(jpg|png|jpeg)$/,
+	loaders: [
+		'file-loader',
+		'image-webpack-loader'
+	]
+      },
+      {
+        test:/\.scss$/,
+        loaders:[
+               'style-loader',
+       	       'css-loader',
+	       'sass-loader',
+	       'autoprefixer-loader'
+        ]
+      }
+    ]
+  }	
 };
